a8c2b5b25850b245499a79ddc251477a95ba23fe
王大宸 <240216240@qq.com>
2023-02-11 14:29:24 +0800

    first commit

---
+++ fly-gateway/src/main/java/com.iidachen.gateway/GatewayBootstrap.java
@@ -0,0 +1,10 @@
+package com.iidachen.gateway;
+
+/**
+ * Description: 网关启动入口类
+ * Created by 王大宸 on 2023/02/11 14:20
+ * Created with IntelliJ IDEA.
+ */
+public class GatewayBootstrap {
+
+}
---
+++ fly-gateway/src/test/java/com/iidachen/gateway/GatewayBootstrapTests.java
@@ -0,0 +1,9 @@
+package com.iidachen.gateway;
+
+/**
+ * Description:
+ * Created by 王大宸 on 2023/02/11 14:20
+ * Created with IntelliJ IDEA.
+ */
+public class GatewayBootstrapTests {
+}
