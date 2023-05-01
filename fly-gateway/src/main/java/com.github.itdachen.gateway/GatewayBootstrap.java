fba08d6202411df629ca60bb08def7ba9a4e8ee9
王大宸 <240216240@qq.com>
2023-05-01 16:40:14 +0800

    认证客户端

--- fly-gateway/src/main/java/com.github.itdachen.gateway/GatewayBootstrap.java
+++ fly-gateway/src/main/java/com.github.itdachen.gateway/GatewayBootstrap.java
@@ -1,10 +1,31 @@
 package com.github.itdachen.gateway;
 
+import com.github.itdachen.framework.boot.runner.SpringBootBootstrap;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
  * Description: 网关启动入口类
  * Created by 王大宸 on 2023/02/11 14:20
  * Created with IntelliJ IDEA.
  */
+@EnableAsync
+@RefreshScope
+@EnableScheduling
+// @EnableFlyAuthClient
+@EnableDiscoveryClient
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.github.itdachen"})
+@EnableFeignClients({"com.github.itdachen"})
 public class GatewayBootstrap {
+
+    public static void main(String[] args) {
+        SpringBootBootstrap.run(GatewayBootstrap.class);
+    }
 
 }
