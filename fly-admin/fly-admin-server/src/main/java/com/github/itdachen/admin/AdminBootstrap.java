9c3b94bce3c797808f8656fea35b25087c8ba2ad
王大宸 <240216240@qq.com>
2023-05-05 21:31:00 +0800

    认证客户端

--- fly-admin/fly-admin-server/src/main/java/com/github/itdachen/admin/AdminBootstrap.java
+++ fly-admin/fly-admin-server/src/main/java/com/github/itdachen/admin/AdminBootstrap.java
@@ -5,6 +5,7 @@ import com.github.itdachen.framework.boot.runner.SpringBootBootstrap;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -19,8 +20,9 @@ import tk.mybatis.spring.annotation.MapperScan;
 @EnableFlyAuthClient
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients({"com.github.itdachen"})
 @ComponentScan(basePackages = {"com.github.itdachen"})
-@MapperScan(basePackages = "com.github.itdachen.auth.**.mapper")
+@MapperScan(basePackages = "com.github.itdachen.admin.**.mapper")
 public class AdminBootstrap {
 
     public static void main(String[] args) {
