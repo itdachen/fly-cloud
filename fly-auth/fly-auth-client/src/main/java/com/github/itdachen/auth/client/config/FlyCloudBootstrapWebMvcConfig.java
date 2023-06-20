2e8155547a27f28f0c2e0c54f43e4c74c93817e0
王大宸 <240216240@qq.com>
2023-06-20 23:11:34 +0800

    提交

--- fly-auth/fly-auth-client/src/main/java/com/github/itdachen/auth/client/config/FlyCloudBootstrapWebMvcConfig.java
+++ fly-auth/fly-auth-client/src/main/java/com/github/itdachen/auth/client/config/FlyCloudBootstrapWebMvcConfig.java
@@ -8,6 +8,7 @@ import com.github.itdachen.auth.jwts.parse.resolver.UserAuthRestMethodArgumentRe
 import com.github.itdachen.framework.body.advice.handler.GlobalExceptionHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
