ce9cc6fde4cc6521e9caeb957f2e8efd691ef74f
王大宸 <240216240@qq.com>
2023-07-16 00:56:02 +0800

    网关修改

--- fly-gateway/src/main/java/com/github/itdachen/gateway/service/impl/GatewayIgnoreRequest.java
+++ fly-gateway/src/main/java/com/github/itdachen/gateway/service/impl/GatewayIgnoreRequest.java
@@ -1,7 +1,11 @@
 package com.github.itdachen.gateway.service.impl;
 
+import com.github.itdachen.framework.autoconfigure.cloud.gateway.ignore.FlyGatewayIgnoreProperties;
 import com.github.itdachen.gateway.service.IgnoreRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Description: 不需要认证的请求
@@ -11,9 +15,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class GatewayIgnoreRequest implements IgnoreRequest {
 
+    private final FlyGatewayIgnoreProperties gatewayIgnoreProperties;
+
+    public GatewayIgnoreRequest(FlyGatewayIgnoreProperties gatewayIgnoreProperties) {
+        this.gatewayIgnoreProperties = gatewayIgnoreProperties;
+    }
+
     @Override
     public Boolean verifyUriMatchers(String uri) {
-        return null;
-
+        List<String> matchers = gatewayIgnoreProperties.getMatchers();
+        if (matchers.contains(uri)) {
+            return true;
+        }
+        return true;
     }
+
 }
