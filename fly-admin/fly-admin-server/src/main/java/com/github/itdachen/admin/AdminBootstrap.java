ef736f34367a0e8dd7e04d89c1065879c1a4a6bd
王大宸 <240216240@qq.com>
2023-12-10 21:13:39 +0800

    远程调用由 feign 修改成 dubbo

--- fly-admin/fly-admin-server/src/main/java/com/github/itdachen/admin/AdminBootstrap.java
+++ fly-admin/fly-admin-server/src/main/java/com/github/itdachen/admin/AdminBootstrap.java
@@ -5,7 +5,6 @@ import com.github.itdachen.framework.boot.runner.SpringBootBootstrap;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -20,7 +19,6 @@ import tk.mybatis.spring.annotation.MapperScan;
 @EnableAuthClient
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableFeignClients({"com.github.itdachen"})
 @ComponentScan(basePackages = {"com.github.itdachen"})
 @MapperScan(basePackages = "com.github.itdachen.admin.**.mapper")
 public class AdminBootstrap {
